eb2efb781bf5a78ae2a708beb974cce764d8e73c
gadapooja <pooja.gada@pjtpartners.com>
2022-07-12 12:50:55 -0400

    updated image code

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 
 import streamlit as st
 
-st.text('Welcome to the PJT web portal')
+st.text('Welcome to the web application for ')
 
 image = (r"C:\\Users\\pooja.gada\\Downloads\\pjt_logo.jpg")
-st.image(image)
+st.image(image,caption = 'PJT Logo')
 with st.form(key='my_form'):
     username = st.text_input('Username')
     password = st.text_input('Password')
     submitted = st.form_submit_button("Login")
-    if submitted:
-        if username == 'pooja.gada@pjtpartners.com':
-            st.success("User logged in")
-    else:
-        st.error('You do not have access to this webpage')
-        st.stop()
+if submitted:
+    if username == 'pooja.gada@pjtpartners.com':
+        st.success("User logged in")
+else:
+    st.error('You do not have access to this webpage')
+    st.stop()
